ca0b989bca8534a33beca0edde054f11794c5944
Prakhar Verma <prakharverma1022@gmail.com>
2024-05-12 17:53:22 +0530

    updated kruskal algo

--- DAA/Greedy/kruskal_algo.cpp
+++ DAA/Greedy/kruskal_algo.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 #include <queue>
 #include <algorithm>
+#define V 9
+
 using namespace std;
 
 class Graph {
-public:
+    public:
     unordered_map<int, vector<pair<int, int>>> adj;
-
     void insert(int u, int v, int w) {
         adj[u].push_back({v, w});
         adj[v].push_back({u, w});
@@ -18,7 +19,7 @@ public:
         for(auto i : adj) {
             cout << i.first << " --> ";
             for(auto j : i.second) {
-                cout << "(" << j.first << ", " << j.second << ") ";
+                cout << "[" << j.first << ", " << j.second << "] ";
             }
             cout << endl;
         }
@@ -26,10 +27,9 @@ public:
 };
 
 class DisjointSet {
-    vector<int> rank, parent, size;
-public:
+    vector<int> size, parent;
+    public:
     DisjointSet(int n) {
-        rank.resize(n+1, 0);
         size.resize(n+1, 1);
         parent.resize(n+1);
 
@@ -38,35 +38,21 @@ public:
         }
     }
 
-    int findUltimateParent(int node) {
-        if(node == parent[node])
+    int findUltParent(int node) {
+        if(parent[node] == node) {
             return node;
-        return parent[node] = findUltimateParent(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUltimateParent(u);
-        int ulp_v = findUltimateParent(v);
-
-        if(ulp_u == ulp_v) return;
-        if(rank[ulp_u] < rank[ulp_v]) parent[ulp_u] = ulp_v;
-        else if(rank[ulp_v] < rank[ulp_u]) parent[ulp_v] = ulp_u;
-        else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
         }
+        return parent[node] = findUltParent(parent[node]);
     }
 
     void unionBySize(int u, int v) {
-        int ulp_u = findUltimateParent(u);
-        int ulp_v = findUltimateParent(v);
+        int ulp_u = findUltParent(u);
+        int ulp_v = findUltParent(v);
 
-        if(ulp_u == ulp_v) return;
         if(size[ulp_u] < size[ulp_v]) {
             parent[ulp_u] = ulp_v;
             size[ulp_v] += size[ulp_u];
-        }
-        else {
+        } else {
             parent[ulp_v] = ulp_u;
             size[ulp_u] += size[ulp_v];
         }
@@ -74,29 +60,29 @@ public:
 };
 
 class Kruskal {
-public:
-    int spanningTree(int V, unordered_map<int, vector<pair<int, int>>>& adj) {
-        vector<pair<int, pair<int, int>>> edges;
+    vector<pair<int, pair<int, int>>> edges;
+    public:
+    int spanningTree(unordered_map<int, vector<pair<int, int>>> &adj) {
         for(auto i : adj) {
             int node = i.first;
-            for(auto it : i.second) {
-                int adjNode = it.first;
-                int wt = it.second;
+            for(auto j : i.second) {
+                int adjNode = j.first;
+                int weight = j.second;
 
-                edges.push_back({wt, {node, adjNode}});
+                edges.push_back({weight, {node, adjNode}});
             }
         }
 
         DisjointSet ds(V);
-
         sort(edges.begin(), edges.end());
         int mstWt = 0;
-        for(auto it : edges) {
-            int wt = it.first;
-            int u = it.second.first;
-            int v = it.second.second;
 
-            if(ds.findUltimateParent(u) != ds.findUltimateParent(v)) {
+        for(auto i : edges) {
+            int wt = i.first;
+            int u = i.second.first;
+            int v = i.second.second;
+
+            if(ds.findUltParent(u) != ds.findUltParent(v)) {
                 mstWt += wt;
                 ds.unionBySize(u, v);
             }
@@ -125,9 +111,8 @@ int main() {
 
     G.print();
 
-    int n = 9;
     Kruskal K;
-    int mstWeight = K.spanningTree(n, G.adj);
-    cout << "MST Weight: " << mstWeight << endl;
+    int minWt = K.spanningTree(G.adj);
+    cout << "The minimum spanning tree weight is: " << minWt << endl;
     return 0;
 }
