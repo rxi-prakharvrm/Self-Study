7426ac9899932fc791e0844c21ba5efc771d65c4
Prakhar Verma <68277409+rxi-prakharvrm@users.noreply.github.com>
2024-10-11 20:08:55 +0530

    Update selectionSort.cpp

--- Myself_DAA/SortingAlgorithms/selectionSort.cpp
+++ Myself_DAA/SortingAlgorithms/selectionSort.cpp
@@ -1,13 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// ============= alternate way ================
+// void selectionSort(vector<int> &arr, int n) {
+//     for(int i = 0; i < n-1; i++) {
+//         for(int j = i+1; j < n; j++) {
+//             if(arr[i] > arr[j]) {
+//                 swap(arr[i], arr[j]);
+//             }
+//         }
+//     }
+// }
+
 void selectionSort(vector<int> &arr, int n) {
     for(int i = 0; i < n-1; i++) {
+        int minIndex = i;
         for(int j = i+1; j < n; j++) {
-            if(arr[i] > arr[j]) {
-                swap(arr[i], arr[j]);
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
             }
         }
+        swap(arr[i], arr[minIndex]);
     }
 }
 
